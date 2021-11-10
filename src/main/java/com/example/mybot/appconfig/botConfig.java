cf448eedcad223ed5a8dd3f38047e329763b7fed
hamzahPrasetio <prasetiohamzah@gmail.com>
2021-11-10 16:48:19 +0700

    Initial commit

---
+++ src/main/java/com/example/mybot/appconfig/botConfig.java
@@ -0,0 +1,41 @@
+package com.example.mybot.appconfig;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.meta.ApiContext;
+import com.example.mybot.bot;
+
+@Setter
+@Getter
+@Configuration
+@ConfigurationProperties(prefix = "telegrambot")
+public class botConfig {
+    private String webHookPath;
+    private String botUserName;
+    private String botToken;
+
+    private DefaultBotOptions.ProxyType proxyType;
+    private String proxyHost;
+    private int proxyPort;
+
+    @Bean
+    public bot MySuperTelegramBot() {
+        DefaultBotOptions options = ApiContext
+                .getInstance(DefaultBotOptions.class);
+
+//        options.setProxyHost(proxyHost);
+//        options.setProxyPort(proxyPort);
+//        options.setProxyType(proxyType);
+
+        bot mySuperTelegramBot = new bot(options);
+        mySuperTelegramBot.setBotUserName(botUserName);
+        mySuperTelegramBot.setBotToken(botToken);
+        mySuperTelegramBot.setWebHookPath(webHookPath);
+
+        return mySuperTelegramBot;
+    }
+}
---
+++ src/main/java/com/example/mybot/controller/WebHookController.java
@@ -0,0 +1,25 @@
+package com.example.mybot.controller;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import com.example.mybot.bot;
+
+
+@RestController
+public class WebHookController {
+    private final bot telegramBot;
+
+    public WebHookController(bot telegramBot) {
+        this.telegramBot = telegramBot;
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public BotApiMethod<?> onUpdateReceived(@RequestBody Update update) {
+        return telegramBot.onWebhookUpdateReceived(update);
+    }
+
+}
